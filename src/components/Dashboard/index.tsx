ad12b30ef6e53dd711a55a4d058dc9605c863397
Hudson Freitas <hudson223@gmail.com>
2022-01-18 16:46:03 -0300

    feat: component transaction

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from '../Summary';
+import { TransactionTable } from '../TransactionsTable';
 import * as S from './styles';
 
 export function Dashboard() {
     return (
         <S.Container>
             <Summary />
+            <TransactionTable/>
         </S.Container>
     )
 }
