fa7a1d73e8d9457aece643fdbf233ae5316fa797
hudson <hudson223@gmail.com>
2022-01-18 23:00:28 -0300

    feat: modal buttons styling

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -38,6 +38,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                   type="button"
                   onClick={() => setType('deposit')}
                   isActive={type === 'deposit'}
+                  activeColor="green"
                 >
                     <img src={incomeImg} alt="Entrada" />
                     <span>Entrada</span>
@@ -47,6 +48,7 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
                   type="button"
                   onClick={() => setType('withdraw')}
                   isActive={type === 'withdraw'}
+                  activeColor="red"
                 >
                     <img src={outcomeImg} alt="Saída" />
                     <span>Saída</span>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {darken} from "polished"
+import {darken, transparentize} from "polished"
 
 export const Container = styled.form`
     h2 {
@@ -54,13 +54,21 @@ export const TransactionTypeContainer = styled.div`
 
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+
+const colors = {
+    green: '#33cc95',
+    red: '#e52e4d'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
     border: 1px solid #d7d7d7;
     border-radius: 0.25rem;
-    background: ${props => props.isActive ? '#ccc' : 'transparent'};
+    background: ${props => props.isActive 
+    ? transparentize(0.9, colors[props.activeColor])
+    : 'transparent'};
     display: flex;
     align-items: center;
     justify-content: center;
